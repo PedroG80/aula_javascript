5b04ebb8f2ed0669f0873e93574b6df359164249
Pedro Granja Nogueira <103385518+PedroG80@users.noreply.github.com>
2022-04-25 20:32:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+/*let d = new Date();
+//alert(d);
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+alert('msg');
+
+/* outra estrutura de repetição utilizando "for"
+let count;
+for (count = 0; count <= 5; count++) {
+  alert(count);
+};
+*/
+/* permite fazer uma estrutura de repetição utilizando o "while"
+let count = 0;
+while (count <= 5) {
+  console.log(count);
+  count = count + 1;
+}
+*/
+/* O comando prompt faz com o browser(navegador de internet) faz uma pergunta ao usuário, a respósta e gravada na variável, o que permite fazer uma comparação.
+var idade = prompt("Informe sua idade");
+if (idade >=18) {
+  alert("Ok! você é maior de idade");
+}else {
+  alert("Conteúdo inapropriado, você não pode acessar essa página")
+}
+*/
+/*var fruta = {nome: "maça", cor:"vermelha"}
+console.log(fruta);//cria uma estrutura para os dados similar a um dicionário
+*/
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva"); // acrescenta um elemento a lista
+//lista.pop(); //tira o último elemento da lista
+//console.log(lista);
+//console.log(lista.join(" - ")); //sustitui a virgula da Array e transforma em string e coloca o por join
+//console.log(lista.toString());//transforma a lista de Array em uma string
+//console.log(lista.reverse());//inverte a ordem de apresentação dos elementos da lista
+//console.log(lista.length);//.length mosta a quantidade de elementos que tem na lista
+
+/*var nome = " Pedro Granja ";
+var idade = 31;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLowerCase());
+*/
+
+//console.log(frase.toUpperCase());
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(nome + " tem " + (idade + idade2) + " anos " );
+// alert(nome + " tem " + idade + " anos ");
